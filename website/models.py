bd56c6801ebe277d6954d3ce5354c9649f1335d0
Tim Lyons <amazingasriel2001@gmail.com>
2023-02-27 21:29:37 -0800

    Search Engine Only displays unique values, complexes are listing as <p>

--- website/models.py
+++ website/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Cities(models.Model):
     name = models.CharField(max_length=28)
+    complex_name = models.CharField(max_length=64)
 
     def __str__(self):
         return f"{self.name}"
