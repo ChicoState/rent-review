08e804594d9d78e71cb8e5090f2e39fe832a33fc
Tim Lyons <amazingasriel2001@gmail.com>
2023-03-04 15:46:43 -0800

    Working csv file input

--- website/models.py
+++ website/models.py
@@ -6,6 +6,9 @@ from django.db import models
 class Cities(models.Model):
     name = models.CharField(max_length=28)
     complex_name = models.CharField(max_length=64)
+    address = models.CharField(max_length=64)
+    url = models.CharField(max_length=128)
+    zipcode = models.IntegerField()
 
     def __str__(self):
         return f"{self.name}"
