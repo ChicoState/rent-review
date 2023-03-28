292d3fab62a0b46d7dd201d884f6c586e297f7bf
Tim Lyons <amazingasriel2001@gmail.com>
2023-03-27 21:40:57 -0700

    Added Post and Comments Class

--- website/models.py
+++ website/models.py
@@ -14,3 +14,29 @@ class Cities(models.Model):
         return f"{self.name}"
 
 
+class Posts(models.Model):
+    # Look into this?
+    # https://pypi.org/project/django-star-ratings/
+    stars = (
+        (1, "One Star")
+        (2, "Two Star")
+        (3, "Three Star")
+        (4, "Four Star")
+        (5, "Five Star")
+    )
+    ComplexID = models.ForeignKey(Cities, on_delete=models.CASCADE)
+    PostTitle = models.CharField(max_length=128)
+    PostText = models.CharField(max_length=1028)
+    likes = models.IntegerField(max_length=10, default=0)
+    strictness = models.IntegerField(choices=stars)
+    Amennities = models.IntegerField(choices=stars)
+    Accessibility = models.IntegerField(choices=stars)
+    Maintenence = models.IntegerField(choices=stars)
+    GracePeriod = models.IntegerField(choices=stars)
+    StaffFriendlyness = models.IntegerField(choices=stars)
+    Price = models.IntegerField(choices=stars)
+
+
+class Comments(models.Model):
+    PostID = models.ForeignKey(Posts, on_delete=models.CASCADE)
+    CommentText = models.CharField(max_length=512)
