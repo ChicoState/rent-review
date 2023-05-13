9fefe8a2eb5710b48ffd9dce728f11645c581f55
Tim Lyons <amazingasriel2001@gmail.com>
2023-05-13 14:48:24 -0700

    Added Use Message and Started Test Cases

--- rentreview/settings.py
+++ rentreview/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
 
 # Use nose to run all tests
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-# Tell nose to measure coverage on the 'foo' and 'bar' apps
+# Tell nose to measure coverage on website app
+#python manage.py test --with-coverage
 NOSE_ARGS = [
     '--with-coverage',
     '--cover-package=website',
--- website/tests.py
+++ website/tests.py
@@ -1,3 +1,51 @@
 from django.test import TestCase
+from website.models import State, City, Complex, Posts, Comments
+from website.views import home, cityLookup, complexLookup, postLookup, add_post, join, user_login, user_logout, createComplex,update_latlng, extract_lat_long_via_address, load_cities
 
 # Create your tests here.
+
+class TestStateModel(TestCase):
+    def setUp(self):
+        State.objects.create(name = "California")
+        
+    def test_State_Object(self):
+        CA = State.objects.get(name = "California")
+        self.assertIsNotNone(CA)
+
+class TestCityModel(TestCase):
+    pass
+
+class TestComplexModel(TestCase):
+    pass
+
+class TestPostsModel(TestCase):
+    pass
+
+class TestCommetsModel(TestCase):
+    pass
+
+
+class TestHomeView(TestCase):
+    pass
+class TestcityLookupView(TestCase):
+    pass
+class TestComplexLookupView(TestCase):
+    pass
+class TestPostLookupView(TestCase):
+    pass
+class TestAdd_PostView(TestCase):
+    pass
+class TestJoinView(TestCase):
+    pass
+class TestUser_LoginView(TestCase):
+    pass
+class TestUser_LogoutView(TestCase):
+    pass
+class TestCreateComplexView(TestCase):
+    pass
+class TestUpdate_LatlngView(TestCase):
+    pass
+class TestExtract_lat_long_via_addressView(TestCase):
+    pass
+class TestLoad_citiesView(TestCase):
+    pass
