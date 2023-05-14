a89785f5a407615fd0e8be95aad1e2e03ee7958b
Tim Lyons <amazingasriel2001@gmail.com>
2023-05-13 17:37:38 -0700

    Load Cities Test Complete

--- website/tests.py
+++ website/tests.py
@@ -1,79 +1,89 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from website.models import State, City, Complex, Posts, Comments
-from website.views import home, cityLookup, complexLookup, postLookup, add_post, join, user_login, user_logout, createComplex,update_latlng, extract_lat_long_via_address, load_cities
+from website.views import home, cityLookup, complexLookup, postLookup, add_post, join, user_login, user_logout, createComplex,update_latlng, extract_lat_long_via_address, load_cities, init_testSet
 from django.urls import reverse
+
+
 # Create your tests here.
 
-class StateTestCase(TestCase):
+class StateTest(TestCase):
     def setUp(self):
         State.objects.create(name = "California")
-        
-    def test_State_Object(self):
+    def test_StateObject(self):
         CA = State.objects.get(name = "California")
         self.assertIsNotNone(CA)
-        
-    def test_smokefail(self):
-        self.assertTrue(False)
 
-class TestCityModel(TestCase):
-    pass
+# class TestCityModel(TestCase):
+#     pass
 
-class TestComplexModel(TestCase):
-    pass
+# class TestComplexModel(TestCase):
+#     pass
 
-class TestPostsModel(TestCase):
-    pass
+# class TestPostsModel(TestCase):
+#     pass
 
-class TestCommetsModel(TestCase):
-    pass
+# class TestCommetsModel(TestCase):
+#     pass
 
 
-class TestHomeView(TestCase):
+# class TestHomeView(TestCase):
+#     pass
+#     # @classmethod
+#     # def setUpTestData(cls):
+#     #     # Create 13 authors for pagination tests
+#     #     number_of_authors = 13
 
-    @classmethod
-    def setUpTestData(cls):
-        # Create 13 authors for pagination tests
-        number_of_authors = 13
+#     #     for author_id in range(number_of_authors):
+#     #         Author.objects.create(
+#     #             first_name=f'Dominique {author_id}',
+#     #             last_name=f'Surname {author_id}',
+#     #         )
 
-        for author_id in range(number_of_authors):
-            Author.objects.create(
-                first_name=f'Dominique {author_id}',
-                last_name=f'Surname {author_id}',
-            )
+#     # def test_view_url_exists_at_desired_location(self):
+#     #     response = self.client.get('/catalog/authors/')
+#     #     self.assertEqual(response.status_code, 200)
 
-    def test_view_url_exists_at_desired_location(self):
-        response = self.client.get('/catalog/authors/')
+#     # def test_view_url_accessible_by_name(self):
+#     #     response = self.client.get(reverse('authors'))
+#     #     self.assertEqual(response.status_code, 200)
+
+#     # def test_view_uses_correct_template(self):
+#     #     response = self.client.get(reverse('authors'))
+#     #     self.assertEqual(response.status_code, 200)
+#     #     self.assertTemplateUsed(response, 'catalog/author_list.html')
+
+
+# class TestcityLookupView(TestCase):
+#     pass
+# class TestComplexLookupView(TestCase):
+#     pass
+# class TestPostLookupView(TestCase):
+#     pass
+# class TestAdd_PostView(TestCase):
+#     pass
+# class TestJoinView(TestCase):
+#     pass
+# class TestUser_LoginView(TestCase):
+#     pass
+# class TestUser_LogoutView(TestCase):
+#     pass
+# class TestCreateComplexView(TestCase):
+#     pass
+# class TestUpdate_LatlngView(TestCase):
+#     pass
+# class TestExtract_lat_long_via_addressView(TestCase):
+#     pass
+class LoadCityTest(TestCase):
+    def setUp(self):
+        state = State.objects.create(name = "California")
+        City.objects.create(name = 'Chico', state = state)
+        City.objects.create(name = 'Roseville', state = state)
+        self.id = State.objects.get(name = 'California').pk
+
+    def test_LoadCities(self):
+        response = self.client.get(reverse('ajax_load_cities'), data={'state':self.id,})
         self.assertEqual(response.status_code, 200)
-
-    def test_view_url_accessible_by_name(self):
-        response = self.client.get(reverse('authors'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_view_uses_correct_template(self):
-        response = self.client.get(reverse('authors'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'catalog/author_list.html')
+        self.assertTrue('cities' in response.context)
+        self.assertEqual(len(response.context['cities']), 2)
 
 
-class TestcityLookupView(TestCase):
-    pass
-class TestComplexLookupView(TestCase):
-    pass
-class TestPostLookupView(TestCase):
-    pass
-class TestAdd_PostView(TestCase):
-    pass
-class TestJoinView(TestCase):
-    pass
-class TestUser_LoginView(TestCase):
-    pass
-class TestUser_LogoutView(TestCase):
-    pass
-class TestCreateComplexView(TestCase):
-    pass
-class TestUpdate_LatlngView(TestCase):
-    pass
-class TestExtract_lat_long_via_addressView(TestCase):
-    pass
-class TestLoad_citiesView(TestCase):
-    pass
